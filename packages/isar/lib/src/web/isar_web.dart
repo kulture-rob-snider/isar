24736fab1ebf7563084fecdd787a3fe9b966e033
Rob Snider <rob@kulture.com>
2022-09-09 01:59:46 -0400

    fix web build error for isarAutoIncrementId not being a const

--- packages/isar/lib/src/web/isar_web.dart
+++ packages/isar/lib/src/web/isar_web.dart
@@ -12,7 +12,7 @@ import 'package:meta/meta.dart';
 
 const Id isarMinId = -9007199254740991;
 const Id isarMaxId = 9007199254740991;
-final Id isarAutoIncrementId = double.negativeInfinity as int;
+const Id isarAutoIncrementId = -9007199254740992;
 
 Never unsupportedOnWeb() {
   throw UnsupportedError('This operation is not supported for Isar web');
